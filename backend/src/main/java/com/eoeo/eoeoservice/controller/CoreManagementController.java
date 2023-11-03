49134dfbc6b6dec85644738af94aa49d75020135
Skyline5555 <john3176@dongguk.edu>
2023-11-03 16:09:22 +0900

    Removed @RequestParam annotation from few controllers.

--- backend/src/main/java/com/eoeo/eoeoservice/controller/CoreManagementController.java
+++ backend/src/main/java/com/eoeo/eoeoservice/controller/CoreManagementController.java
@@ -6,7 +6,6 @@ import com.eoeo.eoeoservice.service.CoreManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,13 +17,13 @@ public class CoreManagementController {
     private final CoreManagementService coreManagementService;
 
     @PostMapping("/addschool")
-    public Long addSchool(@RequestParam AddSchoolRequestDto request){
-        System.out.println(request.getSchoolName());
+    public Long addSchool(AddSchoolRequestDto request){
+        System.out.println(request.getName());
         return coreManagementService.addSchool(request);
     }
 
     @PostMapping("/addcorelecturetype")
-    public Long addCoreLectureType(@RequestParam AddCoreLectureTypeRequestDto request){
+    public Long addCoreLectureType(AddCoreLectureTypeRequestDto request){
         return coreManagementService.addCoreLectureType(request);
     }
 
--- backend/src/main/java/com/eoeo/eoeoservice/controller/CourseManagementController.java
+++ backend/src/main/java/com/eoeo/eoeoservice/controller/CourseManagementController.java
@@ -1,12 +1,12 @@
 package com.eoeo.eoeoservice.controller;
 
+import com.eoeo.eoeoservice.dto.course_management.AddCoreCourseCreditRequestDto;
 import com.eoeo.eoeoservice.dto.course_management.AddCoreCourseRequestDto;
 import com.eoeo.eoeoservice.dto.course_management.AddCourseRequestDto;
 import com.eoeo.eoeoservice.service.CourseManagementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,13 +17,18 @@ public class CourseManagementController {
     private final CourseManagementService courseManagementService;
 
     @PostMapping("/addcourse")
-    public Long addCourse(@RequestParam AddCourseRequestDto request){
+    public Long addCourse(AddCourseRequestDto request){
         return courseManagementService.addCourse(request);
     }
 
     @PostMapping("/addcorecourse")
-    public Long addCoreCourse(@RequestParam AddCoreCourseRequestDto request){
+    public Long addCoreCourse(AddCoreCourseRequestDto request){
         return courseManagementService.addCoreCourse(request);
+    }
+
+    @PostMapping("/addcorecoursecredit")
+    public Long addCoreCourseCredit(AddCoreCourseCreditRequestDto request){
+        return courseManagementService.addCoreCourseCredit(request);
     }
 
 }
