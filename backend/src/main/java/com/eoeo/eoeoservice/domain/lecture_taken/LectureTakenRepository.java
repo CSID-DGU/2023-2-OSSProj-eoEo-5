49914a2ac69be95108ed6e960f0034e3bbd4e4c0
Skyline5555 <john3176@dongguk.edu>
2023-11-03 18:03:57 +0900

    Typo fix

--- backend/src/main/java/com/eoeo/eoeoservice/domain/lecture_taken/LectureTakenRepository.java
+++ backend/src/main/java/com/eoeo/eoeoservice/domain/lecture_taken/LectureTakenRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LectureTakenRespository extends JpaRepository<LectureTaken, Long> {
+public interface LectureTakenRepository extends JpaRepository<LectureTaken, Long> {
 }
