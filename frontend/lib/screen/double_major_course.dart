ce7fae2162bf0d435d6521ba80e1387e4ce0016d
NANGEUN <klemon@dgu.ac.kr>
2023-11-04 17:51:51 +0900

    복수전공 이수체계도 페이지 생성 및 레이아웃

---
+++ frontend/lib/screen/double_major_course.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class double_major_course extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        title: Text(
+          '복수전공 : 융합소프트웨어학과 이수체계도 보기',
+          style: TextStyle(
+            color: Colors.white,
+            //fontWeight: FontWeight.bold,
+          ),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '전공필수',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Divider(), // 구분선 추가
+
+            ListTile(
+              title: Text('필수과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
+              onTap: () {
+              },
+            ),
+            ListTile(
+              title: Text('필수과목2'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
+              onTap: () {
+              },
+            ),
+            ListTile(
+              title: Text('선택과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
+              onTap: () {
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
