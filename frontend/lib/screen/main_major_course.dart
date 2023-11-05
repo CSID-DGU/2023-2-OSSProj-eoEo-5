8fc775542061556839ebaf45fc6454f60465feed
NANGEUN <klemon@dgu.ac.kr>
2023-11-05 12:04:52 +0900

    주전공 스크롤 가능하게 변경 ( 리스트뷰 이용 )

--- frontend/lib/screen/main_major_course.dart
+++ frontend/lib/screen/main_major_course.dart
@@ -10,8 +10,8 @@ class Main_major_course extends StatelessWidget {
           '주전공 : 경영학과 이수체계도 보기',
           style: TextStyle(
             color: Colors.white,
-            //fontWeight: FontWeight.bold,
-          ),),
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -28,25 +28,35 @@ class Main_major_course extends StatelessWidget {
 
             Divider(), // 구분선 추가
 
-            ListTile(
-              title: Text('필수과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-              onTap: () {
-              },
+            // ListView를 사용하여 스크롤 가능한 목록 생성
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('경영학원론'),
+                    onTap: () {
+                      // 경영학원론에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('조직행위'),
+                    onTap: () {
+                      // 조직행위 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('재무관리'),
+                    onTap: () {
+                      // 재무관리 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  // 다른 과목 항목들도 유사하게 추가할 수 있습니다.
+                ],
+              ),
             ),
-            ListTile(
-              title: Text('필수과목2'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-              onTap: () {
-              },
-            ),
-        ListTile(
-          title: Text('선택과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-          onTap: () {
-          },
-        ),
           ],
         ),
       ),
     );
   }
 }
-
