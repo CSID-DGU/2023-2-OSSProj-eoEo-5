d171bf0a51f5f19cab898d6d5b01b136ff6ecc92
Skyline5555 <john3176@dongguk.edu>
2023-11-06 01:32:37 +0900

    Changed AccessToken access time for testing

--- backend/src/main/java/com/eoeo/eoeoservice/security/JwtProvider.java
+++ backend/src/main/java/com/eoeo/eoeoservice/security/JwtProvider.java
@@ -25,7 +25,7 @@ public class JwtProvider {
 
     private String tokenKey = "secretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecret";
 
-    private long expireTimeAccessToken = 2L;
+    private long expireTimeAccessToken = 45L;
 
     private long expireRefreshToken = 14L;
 
@@ -35,7 +35,7 @@ public class JwtProvider {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setExpiration(Date.from(now.toInstant().plus(expireTimeAccessToken, ChronoUnit.HOURS)))
+                .setExpiration(Date.from(now.toInstant().plus(expireTimeAccessToken, ChronoUnit.SECONDS)))
                 .signWith(SignatureAlgorithm.HS256, tokenKey)
                 .compact();
     }
