5f87d5745abbcc7e57465760e634799f60523925
NANGEUN <klemon@dgu.ac.kr>
2023-11-05 12:04:47 +0900

    복수전공 스크롤 가능하게 변경 ( 리스트뷰 이용 )

--- frontend/lib/screen/double_major_course.dart
+++ frontend/lib/screen/double_major_course.dart
@@ -10,8 +10,8 @@ class double_major_course extends StatelessWidget {
           '복수전공 : 융합소프트웨어학과 이수체계도 보기',
           style: TextStyle(
             color: Colors.white,
-            //fontWeight: FontWeight.bold,
-          ),),
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -28,20 +28,49 @@ class double_major_course extends StatelessWidget {
 
             Divider(), // 구분선 추가
 
-            ListTile(
-              title: Text('필수과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-              onTap: () {
-              },
-            ),
-            ListTile(
-              title: Text('필수과목2'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-              onTap: () {
-              },
-            ),
-            ListTile(
-              title: Text('선택과목1'), //들은 학점도 오른쪽 상단에 나오면 좋겠다
-              onTap: () {
-              },
+            // ListView를 사용하여 스크롤 가능한 목록 생성
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('융합프로그래밍1'),
+                    onTap: () {
+                      // 융합프로그래밍1 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('컴퓨터시스템'),
+                    onTap: () {
+                      // 컴퓨터시스템 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('자료구조 및 알고리즘1'),
+                    onTap: () {
+                      // 자료구조 및 알고리즘1 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('융합프로그래밍2'),
+                    onTap: () {
+                      // 융합프로그래밍2 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('오픈소스소프트웨어프로젝트'),
+                    onTap: () {
+                      // 오픈소스소프트웨어프로젝트 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  ListTile(
+                    title: Text('융합캡스톤디자인'),
+                    onTap: () {
+                      // 융합캡스톤디자인 과목에 대한 설명이 나오도록 할 수 있습니다.
+                    },
+                  ),
+                  // 다른 과목 항목들도 유사하게 추가할 수 있습니다.
+                ],
+              ),
             ),
           ],
         ),
@@ -49,4 +78,3 @@ class double_major_course extends StatelessWidget {
     );
   }
 }
-
