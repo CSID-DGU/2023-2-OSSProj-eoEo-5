0b61b1813ee744ca9870752538686f3af7cdedb5
NANGEUN <klemon@dgu.ac.kr>
2023-11-06 11:20:52 +0900

    로그인화면 자간 수정

--- frontend/lib/screen/login.dart
+++ frontend/lib/screen/login.dart
@@ -143,7 +143,7 @@ class _LogInState extends State<LoginScreen> {
 void showSnackBar(BuildContext context, Text text) {
   final snackBar = SnackBar(
     content: text,
-    backgroundColor: Color.fromARGB(255, 112, 48, 48),
+    backgroundColor: Color.fromARGB(255, 112, 50, 50),
   );
 
 // Find the ScaffoldMessenger in the widget tree
