a3d651ef1b6b02d4ca0059f5f33d49bf574367ca
NANGEUN <klemon@dgu.ac.kr>
2023-12-04 13:29:37 +0900

    FAQ_항목_페이지전환_추가

--- frontend/lib/screen/FAQ.dart
+++ frontend/lib/screen/FAQ.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class FAQScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlue,
@@ -10,44 +13,155 @@ class FAQScreen extends StatelessWidget {
           'FAQ',
           style: TextStyle(
             color: Colors.white,
-            //fontWeight: FontWeight.bold,
-          ),),
+          ),
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              '소프트웨어 교육원 FAQ',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    _navigateToFAQPage(context, '지원');
+                  },
+                  child: Text(
+                    '지원',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    _navigateToFAQPage(context, '교과');
+                  },
+                  child: Text(
+                    '교과',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    _navigateToFAQPage(context, '학점인정');
+                  },
+                  child: Text(
+                    '학점인정',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+              ],
             ),
+            SizedBox(height: 16), // 필요에 따라 간격 조절
 
-            Divider(), // 구분선 추가
-
-            ListTile(
-              title: Text('FAQ1'),
-              subtitle: Text('답변'),
-              onTap: () {
-              },
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    _navigateToFAQPage(context, '졸업');
+                  },
+                  child: Text(
+                    '졸업',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+                  ),
+                  onPressed: () {
+                    _navigateToFAQPage(context, '취업');
+                  },
+                  child: Text(
+                    '취업',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('FAQ2'),
-              subtitle: Text('답변'),
-              onTap: () {
-              },
-            ),
-            ListTile(
-              title: Text('FAQ2'),
-              subtitle: Text('답변'),
-              onTap: () {
-              },
-            ),
+            SizedBox(height: 16), // 필요에 따라 간격 조절
           ],
         ),
+      ),
+    );
+  }
+
+  void _navigateToFAQPage(BuildContext context, String category) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FAQCategoryScreen(category)),
+    );
+  }
+}
+
+class FAQCategoryScreen extends StatelessWidget {
+  final String category;
+
+  FAQCategoryScreen(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$category FAQ'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('$category FAQ1'),
+            subtitle: Text('$category에 관한 자주 묻는 질문에 대한 답변'),
+            onTap: () {
+              // FAQ1을 눌렀을 때 처리할 로직 작성
+            },
+          ),
+          ListTile(
+            title: Text('$category FAQ2'),
+            subtitle: Text('$category에 관한 자주 묻는 질문에 대한 답변'),
+            onTap: () {
+              // FAQ2를 눌렀을 때 처리할 로직 작성
+            },
+          ),
+          // 다른 FAQ 항목들도 유사한 방식으로 추가할 수 있습니다.
+        ],
       ),
     );
   }
