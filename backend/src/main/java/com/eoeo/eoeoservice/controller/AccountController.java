c3e75b8eb94b940e85afa3e5502ab4667f1fe817
Skyline5555 <john3176@dongguk.edu>
2023-12-04 11:07:34 +0900

    Added logout api

--- backend/src/main/java/com/eoeo/eoeoservice/controller/AccountController.java
+++ backend/src/main/java/com/eoeo/eoeoservice/controller/AccountController.java
@@ -2,9 +2,11 @@ package com.eoeo.eoeoservice.controller;
 
 import com.eoeo.eoeoservice.dto.account.AccountDataRequestDto;
 import com.eoeo.eoeoservice.dto.account.AccountDataResponseDto;
+import com.eoeo.eoeoservice.dto.account.LogoutRequestDto;
 import com.eoeo.eoeoservice.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class AccountController {
     @GetMapping("/getaccount")
     public AccountDataResponseDto getAccountData(AccountDataRequestDto request){
         return accountService.getAccountData(request);
+    }
+
+    @PostMapping("/logout")
+    public Boolean logout(LogoutRequestDto request){
+        return accountService.logout(request);
     }
 }
