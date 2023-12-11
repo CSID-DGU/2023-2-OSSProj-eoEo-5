b01d1e8e434d233aadde624a278e03a06176d78b
yoounyoungheon <iddyoon@gmail.com>
2023-12-11 22:28:20 +0900

    resolne add&delete screen serstate error

--- frontend/lib/screen/subject_taken.dart
+++ frontend/lib/screen/subject_taken.dart
@@ -22,8 +22,9 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
   bool isDataLoaded = false; // 데이터가 로드되었는지 여부를 나타내는 플래그
   List<Widget> takenLectureWidgets = []; // 컨테이너에 띄울 리스트 위젯
   late List<List> lectureList; // 강의 리스트를 담을 리스트
-  TextEditingController tec = TextEditingController();
+  TextEditingController addtec = TextEditingController();
   TextEditingController substituteTextFieldController = TextEditingController();
+  TextEditingController deletetec = TextEditingController();
 
   // checkbox
   bool? ismajor = false;
@@ -235,7 +236,7 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                           .bold),
                     ),
                     TextField(
-                      controller: tec,
+                      controller: deletetec,
                       textAlign: TextAlign.center,
                       decoration: const InputDecoration(
                         hintText: "학수번호를 입력해주세요 !",
@@ -256,7 +257,7 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                       child: ElevatedButton(
                           onPressed: () async {
                             // 사용자 추가 데이터 불러오기: (비동기)
-                            var deleteValue = await loadDeleteLecture(tec.text);
+                            var deleteValue = await loadDeleteLecture(deletetec.text);
                             deleteData = deleteValue;
                             // 데이터 로드
                             setState(() {
@@ -276,7 +277,6 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                                 }
                                 // 들고 있는 리스트에서의 해당 과목의 리스트 찾기
                                 int cnt = 0;
-
                                 for(int i=0; i<lectureList[0].length; i++){
                                   if(lectureList[0][i]["name"] == deletelectureName){
                                     break;
@@ -288,9 +288,9 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                                     "https://eoeoservice.site/lecture/deletetakenlecture", deleteData, true, true, context
                                 ).then((response) {
                                   if (response?.statusCode == 200) {
+                                    takenLectureWidgets.removeAt(cnt);
                                     Navigator.pop(context);
                                     setState(() {});
-                                    takenLectureWidgets.removeAt(cnt);
                                   } else {
                                     setState(() {});
                                   }
@@ -371,7 +371,7 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                           .bold),
                     ),
                     TextField(
-                      controller: tec,
+                      controller: addtec,
                       textAlign: TextAlign.center,
                       decoration: const InputDecoration(
                         hintText: "학수번호를 입력해주세요 !",
@@ -466,7 +466,7 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                       child: ElevatedButton(
                           onPressed: () async {
                             // 사용자 추가 데이터 불러오기: (비동기)
-                            var addValue = await loadAddLecture(tec.text, substituteTextFieldController.text);
+                            var addValue = await loadAddLecture(addtec.text, substituteTextFieldController.text);
                             addData = addValue;
                             // 데이터 로드
                             setState(() {
@@ -483,9 +483,8 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                                   if (response?.statusCode == 200) {
                                     Map<String, dynamic> responseData = jsonDecode(utf8.decode(response?.bodyBytes as List<int>));
                                     addlectureName = responseData["lectureName"];
-                                    Navigator.pop(context);
-                                    setState(() {});
                                     print(addlectureName); // 새로 추가한 과목의 이름
+
                                     // 리스트 위젯에 추가
                                     takenLectureWidgets.add(
                                         Column(
@@ -504,6 +503,8 @@ class _Subject_takenScreen extends State<Subject_takenScreen> {
                                           ],
                                         )
                                     );
+                                    Navigator.pop(context);
+                                    setState(() {});
                                   } else if(response?.statusCode != 200){
                                     setState((){});
                                   }
